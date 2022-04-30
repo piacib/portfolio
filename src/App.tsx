54267948a4d6e9c990203a2fefc5581251dd925b
Ben Piacitelli <ben.piacitelli@gmail.com>
2022-04-30 07:36:09 -1000

    added Bubbles for ferris wheel

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,82 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import React from "react";
+interface BubbleProps {
+  imageSrc: string;
+  imageAlt: string;
+  text: string;
+}
+const Bubble: React.FC<BubbleProps> = ({ imageSrc, imageAlt, text }) => {
+  return (
+    <li>
+      <div>
+        <img src={imageSrc} alt={imageAlt} />
+      </div>
+      <p>text</p>
+    </li>
+  );
+};
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="orbit">
+        <div className="center-image">
+          <img src="" alt="" />
+          <img src="" alt="" />
+        </div>
+        <ul>
+          <Bubble imageSrc="" imageAlt="" text="text" />
+          <li>
+            <div>
+              <img src="" alt="" />
+            </div>
+            <p>Analytics & Reporting</p>
+          </li>
+          <li>
+            <div>
+              <img src="" alt="" />
+            </div>
+            <p>Time & Attendance</p>
+          </li>
+          <li>
+            <div>
+              <img src="" alt="" />
+            </div>
+            <p>Onboarding</p>
+          </li>
+          <li>
+            <div>
+              <img src="" alt="" />
+            </div>
+            <p>HR Information System</p>
+          </li>
+          <li>
+            <div>
+              <img src="" alt="" />
+            </div>
+            <p>Leaves & Holidays</p>
+          </li>
+          <li>
+            <div>
+              <img src="" alt="" />
+            </div>
+            <p>Exit & Off-boarding</p>
+          </li>
+          <li>
+            <div>
+              <img src="" alt="" />
+            </div>
+            <p>Travel & Requisitions</p>
+          </li>
+          <li>
+            <div>
+              <img src="" alt="" />
+            </div>
+            <p>Disciplinary</p>
+          </li>
+        </ul>
+      </div>
     </div>
   );
 }
