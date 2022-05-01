341d389524cbe2a307e0bc50eb9d6060e410a3eb
Ben Piacitelli <ben.piacitelli@gmail.com>
2022-04-30 19:24:25 -1000

    Created ferris wheel

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,14 @@
 import React from "react";
 import "./App.css";
-import React from "react";
+import "./bubble.css";
+import "./normalize.css";
+import Pokeball from "./assets/3.svg";
+import Fish from "./assets/fish-svgrepo-com.svg";
+import QuestionMark from "./assets/question-mark-svgrepo-com.svg";
+import Weather from "./assets/weather-rain-svgrepo-com.svg";
+import Timer from "./assets/timer-svgrepo-com.svg";
+import Linkedin from "./assets/linkedin-svgrepo-com.svg";
+
 interface BubbleProps {
   imageSrc: string;
   imageAlt: string;
@@ -8,73 +16,40 @@ interface BubbleProps {
 }
 const Bubble: React.FC<BubbleProps> = ({ imageSrc, imageAlt, text }) => {
   return (
-    <li>
+    <li className="bubble">
       <div>
-        <img src={imageSrc} alt={imageAlt} />
+        <img className="bubble-image" src={imageSrc} alt={imageAlt} />
       </div>
-      <p>text</p>
+      <p>{text}</p>
     </li>
   );
 };
-
+const dataArray = [
+  { imageSrc: QuestionMark, imageAlt: "question mark", text: "About Me" },
+  { imageSrc: Pokeball, imageAlt: "pokeball", text: "Pokemon Extension" },
+  { imageSrc: Weather, imageAlt: "storm cloud", text: "Weather" },
+  { imageSrc: Fish, imageAlt: "fish", text: "Fish" },
+  { imageSrc: Timer, imageAlt: "timer", text: "Reddit post timer" },
+  { imageSrc: Linkedin, imageAlt: "", text: "Linkedin" },
+  { imageSrc: Pokeball, imageAlt: "", text: "text" },
+  { imageSrc: Pokeball, imageAlt: "", text: "text" },
+];
 function App() {
   return (
     <div className="App">
       <div className="orbit">
         <div className="center-image">
-          <img src="" alt="" />
-          <img src="" alt="" />
+          <img className="profile-image" />
+          <img className="profile-image yellow" />
         </div>
         <ul>
-          <Bubble imageSrc="" imageAlt="" text="text" />
-          <li>
-            <div>
-              <img src="" alt="" />
-            </div>
-            <p>Analytics & Reporting</p>
-          </li>
-          <li>
-            <div>
-              <img src="" alt="" />
-            </div>
-            <p>Time & Attendance</p>
-          </li>
-          <li>
-            <div>
-              <img src="" alt="" />
-            </div>
-            <p>Onboarding</p>
-          </li>
-          <li>
-            <div>
-              <img src="" alt="" />
-            </div>
-            <p>HR Information System</p>
-          </li>
-          <li>
-            <div>
-              <img src="" alt="" />
-            </div>
-            <p>Leaves & Holidays</p>
-          </li>
-          <li>
-            <div>
-              <img src="" alt="" />
-            </div>
-            <p>Exit & Off-boarding</p>
-          </li>
-          <li>
-            <div>
-              <img src="" alt="" />
-            </div>
-            <p>Travel & Requisitions</p>
-          </li>
-          <li>
-            <div>
-              <img src="" alt="" />
-            </div>
-            <p>Disciplinary</p>
-          </li>
+          {dataArray.map((data) => (
+            <Bubble
+              imageSrc={data.imageSrc}
+              imageAlt={data.imageAlt}
+              text={data.text}
+            />
+          ))}
         </ul>
       </div>
     </div>
