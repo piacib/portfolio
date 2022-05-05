9ca158745fec1d05cdb890ba8b6214d27ade48eb
Ben Piacitelli <ben.piacitelli@gmail.com>
2022-05-05 10:37:48 -1000

    added media quesries and image toggle

--- src/components/CenterImage/CenterImage.tsx
+++ src/components/CenterImage/CenterImage.tsx
@@ -1,14 +1,33 @@
 import React from "react";
 import "./CenterImage.css";
+import "./CenterImage.mediaquery.css";
 interface Props {
   src1: string;
-  src2: string;
+  href1: string;
+  href2: string;
+  imageSrc: string;
+  imageAlt: string;
 }
-const CenterImage: React.FC<Props> = ({ src1, src2 }) => {
+const CenterImage: React.FC<Props> = ({
+  src1,
+  imageSrc,
+  imageAlt,
+  href1,
+  href2,
+}) => {
+  const imgZindex = !href1 ? "" : "background";
   return (
     <div className="center-image">
-      <img src={src1} alt="profile" className="profile-image" />
-      <img src={src2} alt="profile" className="profile-image yellow" />
+      <div className="children profile-image yellow options">
+        {href1 ? <a href={href1}>Repository</a> : null}
+        <img src={imageSrc} alt={imageAlt} />
+        {href2 ? <a href={href2}>Demo</a> : null}
+      </div>
+      <img
+        src={src1}
+        alt="profile"
+        className={`${imgZindex} children profile-image`}
+      />
     </div>
   );
 };
