45aaf9a80d60981ab369f7f2bfaf4b1e6663d3f9
Ben Piacitelli <ben.piacitelli@gmail.com>
2022-05-10 09:04:17 -1000

    added comments

--- src/components/Toggle/Toggle.tsx
+++ src/components/Toggle/Toggle.tsx
@@ -1,10 +1,11 @@
 import React, { useEffect, useState } from "react";
 import "./toggle.css";
 import { setTheme } from "../../utils/theme";
-
+// to adjust toggle size set font-size
 function Toggle() {
   const [togClass, setTogClass] = useState("dark");
   let theme = localStorage.getItem("theme");
+  // check for preferred theme
   useEffect(() => {
     if (localStorage.getItem("theme") === "theme-dark") {
       setTogClass("dark");
@@ -12,6 +13,7 @@ function Toggle() {
       setTogClass("light");
     }
   }, [theme]);
+  // toggle themes
   const handleOnClick = () => {
     if (localStorage.getItem("theme") === "theme-dark") {
       setTheme("theme-light");
