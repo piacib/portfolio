8b72d846cfec1bbe56e49bb88baea47662dfa940
Ben Piacitelli <ben.piacitelli@gmail.com>
2022-05-09 18:36:00 -1000

    added a timeout to toggle centerimage back to image

--- src/components/FerrisWheel/FerrisWheel.tsx
+++ src/components/FerrisWheel/FerrisWheel.tsx
@@ -1,7 +1,8 @@
-import React, { useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import Bubble from "../Bubble/Bubble";
 import CenterImage from "../CenterImage/CenterImage";
 import Me from "../../assets/me.jpg";
+
 type Passengers = {
   imageSrc: string;
   imageAlt: string;
@@ -13,12 +14,14 @@ type Passengers = {
 interface Props {
   dataArray: Passengers[];
 }
+
 type Hrefs = {
   repoHref: string;
   demoHref: string;
   imageSrc: string;
   imageAlt: string;
 };
+
 const FerrisWheel: React.FC<Props> = ({ dataArray }) => {
   const [centerImageData, setCenterImageData] = useState<Hrefs>({
     repoHref: "",
@@ -26,9 +29,36 @@ const FerrisWheel: React.FC<Props> = ({ dataArray }) => {
     imageSrc: "",
     imageAlt: "",
   });
+  const [mouseOnCenterImage] = useState<boolean>(false);
+  const mouseOnCenterImageRef = useRef(mouseOnCenterImage);
+  mouseOnCenterImageRef.current = mouseOnCenterImage;
+
+  const func = (mouseOnCenterImageRef: boolean) => {
+    if (!mouseOnCenterImageRef) {
+      setCenterImageData({
+        repoHref: "",
+        demoHref: "",
+        imageSrc: "",
+        imageAlt: "",
+      });
+    }
+  };
+  useEffect(() => {
+    const clearImage = () =>
+      setTimeout(() => func(mouseOnCenterImageRef.current), 5000);
+    if (centerImageData.repoHref) {
+      clearImage();
+    }
+    return () => {
+      clearTimeout(clearImage());
+    };
+  }, [centerImageData.repoHref, mouseOnCenterImageRef]);
   return (
     <div className="orbit">
       <CenterImage
+        mouseOnCenterImage={(arg1: boolean) =>
+          (mouseOnCenterImageRef.current = arg1)
+        }
         src1={Me}
         imageSrc={centerImageData.imageSrc}
         imageAlt={centerImageData.imageAlt}
@@ -38,14 +68,14 @@ const FerrisWheel: React.FC<Props> = ({ dataArray }) => {
       <ul>
         {dataArray.map((data) => (
           <Bubble
-            onClick={() =>
+            onClick={() => {
               setCenterImageData({
                 repoHref: data.repoHref,
                 imageSrc: data.imageSrc,
                 imageAlt: data.imageAlt,
                 demoHref: data.demoHref,
-              })
-            }
+              });
+            }}
             imageSrc={data.imageSrc}
             imageAlt={data.imageAlt}
             text={data.text}
