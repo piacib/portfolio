9d6f756af75d90ff6800c9e21926bf4910178c4b
Ben Piacitelli <ben.piacitelli@gmail.com>
2022-05-23 20:01:56 -1000

    moved arms to bubble component

--- src/components/Bubble/Bubble.tsx
+++ src/components/Bubble/Bubble.tsx
@@ -16,7 +16,17 @@ const Bubble: React.FC<BubbleProps> = ({
 }) => {
   return (
     <li className="bubble" onClick={onClick}>
-      <div>
+      <div className="arm support-arm">
+        {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((x) => (
+          <div className="bolt"></div>
+        ))}
+      </div>
+      <div className="arm connector-arm">
+        {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((x) => (
+          <div className="bolt"></div>
+        ))}
+      </div>
+      <div className="image-container">
         <img className="bubble-image" src={imageSrc} alt={imageAlt} />
       </div>
       <p>{text}</p>
