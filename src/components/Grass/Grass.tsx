274a3f3e4a8d12f589302ce7c89a457fcb3c207f
Ben Piacitelli <ben.piacitelli@gmail.com>
2022-05-27 15:47:58 -1000

    added keys to blades

--- src/components/Grass/Grass.tsx
+++ src/components/Grass/Grass.tsx
@@ -2,8 +2,8 @@ import React from "react";
 import "./grass.css";
 const Grass = () => {
   let blades = [];
-  for (let i = 0; i < 150; i++) {
-    blades.push(<div className="blade"></div>);
+  for (let i = 0; i < 100; i++) {
+    blades.push(<div key={i} className="blade"></div>);
   }
   return <div className="grass">{blades}</div>;
 };
