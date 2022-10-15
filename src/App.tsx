ffb6e2732bda310e1d40b8ff64692286f42e32f0
Ben Piacitelli <ben.piacitelli@gmail.com>
2022-10-15 08:26:27 -1000

    added sections

--- src/App.tsx
+++ src/App.tsx
@@ -5,17 +5,47 @@ import { keepTheme } from "./utils/theme";
 import FerrisWheel from "./components/FerrisWheel/FerrisWheel";
 import Toggle from "./components/Toggle/Toggle";
 import Grass from "./components/Grass/Grass";
-
+import AboutMe from "./components/AboutMe/AboutMe";
+import Projects from "./components/Projects/Projects";
+// import "./starParrallax.css";
+import "./star.css";
+const StarBackground = () => {
+  let stars = [];
+  for (let i = 0; i < 100; i++) {
+    stars.push(
+      <div
+        key={i}
+        style={{
+          top: `${Math.random() * 2000}px`,
+          left: `${Math.random() * 2000}px`,
+        }}
+        className="star"
+      ></div>
+    );
+  }
+  return (
+    <div className="star_container">
+      <div className='star_sheet' id="stars1"></div>
+      <div className='star_sheet' id="stars2"></div>
+      <div className='star_sheet' id="stars3"></div>
+    </div>
+  );
+};
 function App() {
   useEffect(() => {
     keepTheme();
   });
   return (
-    <div className="App">
-      <Toggle />
-      <FerrisWheel />
-      <Grass />
-    </div>
+    <>
+      <section id="home">
+        <StarBackground />
+        <Toggle />
+        <FerrisWheel />
+      </section>
+      <AboutMe />
+      <Projects />
+      {/* <Grass /> */}
+    </>
   );
 }
 
