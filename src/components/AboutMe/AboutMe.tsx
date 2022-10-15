a75880b31c508fce3ed7abe5048bb21dc5db9f69
Ben Piacitelli <ben.piacitelli@gmail.com>
2022-10-15 08:27:00 -1000

    added about me section

--- src/components/AboutMe/AboutMe.tsx
+++ src/components/AboutMe/AboutMe.tsx
@@ -1,19 +1,27 @@
 import React from "react";
-import ExpandingPage, {
-  ExpandingPageProps,
-} from "../ExpandingPage/ExpandingPage";
-const AboutMe: React.FC<ExpandingPageProps> = ({ expand, setExpand }) => {
+import "./AboutMe.css";
+import Me from "../../assets/me.jpg";
+
+const AboutMe: React.FC = () => {
   return (
-    <ExpandingPage expand={expand} setExpand={setExpand}>
-      <h1>About Me</h1>
-      <p>
+    <section id="about_me">
+      <h1 className="section_header">About Me</h1>
+      <ul className="about_me_skills">
+        <li>Html</li>
+        <li>CSS</li>
+        <li>React</li>
+        <li>JS</li>
+        <li>Python</li>
+      </ul>
+      <img className="self_portrait" src={Me} alt="self portrait" />
+      <p className="about_me_text">
         I am an aspiring front end web developer currently searching for a job.
         I graduated for the University of Washington in 2019 with a degree in
         Physics and Astronomy. Since then I have been teaching myself Html css
         Javascript and react. Currently I am living in Hawaii working on Tour
         boats as a deckhand and swim guide.
       </p>
-    </ExpandingPage>
+    </section>
   );
 };
 export default AboutMe;
