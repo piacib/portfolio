0b9f289b6f3d9ac97578c71c1dee6452cfc523bc
piacib <piacib>
2022-12-01 19:34:18 -1000

    removed unnecessary div

--- src/components/Projects/Projects.tsx
+++ src/components/Projects/Projects.tsx
@@ -8,27 +8,25 @@ interface Props {
 }
 const ProjectLi: React.FC<Props> = ({ project, children }) => {
   return (
-    <li>
-      <div className="project_container">
-        <h2>{project.text}</h2>
-        <div className="project_links">
-          <a href={project.demoHref}>Demo</a>
-          <a href={project.repoHref}>Repo</a>
-        </div>
-        <div className="bottom_container">
-          <img
-            className="project_image"
-            src={project.imageSrc}
-            alt={project.imageAlt}
-          />
-          <div className="project_hover">
-            <h3>Skills</h3>
-            <ul className="project_skills">
-              {project.skills.map((skill) => (
-                <li>{skill}</li>
-              ))}
-            </ul>
-          </div>
+    <li className="project_container">
+      <h2>{project.text}</h2>
+      <div className="project_links">
+        <a href={project.demoHref}>Demo</a>
+        <a href={project.repoHref}>Repo</a>
+      </div>
+      <div className="bottom_container">
+        <img
+          className="project_image"
+          src={project.imageSrc}
+          alt={project.imageAlt}
+        />
+        <div className="project_hover">
+          <h3>Skills</h3>
+          <ul className="project_skills">
+            {project.skills.map((skill) => (
+              <li>{skill}</li>
+            ))}
+          </ul>
         </div>
       </div>
     </li>
