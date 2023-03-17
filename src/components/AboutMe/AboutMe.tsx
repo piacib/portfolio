47919efb529065f4ffb95d0385e049d39c781c5f
piacib <piacib>
2023-03-17 14:59:29 -0700

    added background

--- src/components/AboutMe/AboutMe.tsx
+++ src/components/AboutMe/AboutMe.tsx
@@ -6,8 +6,9 @@ import { AboutMeSectionId } from "../../global";
 const AboutMe: React.FC = () => {
   return (
     <section id={AboutMeSectionId}>
+      <div id='about_me_background'></div>
       <h1 className="section_header">About Me</h1>
-      <img className="self_portrait" src={Me} alt="self portrait" />
+      {/* <img className="self_portrait" src={Me} alt="self portrait" />
       <ul className="about_me_skills">
         <li>Html</li>
         <li>CSS</li>
@@ -17,7 +18,7 @@ const AboutMe: React.FC = () => {
         <li>HTML</li>
         <li>Python</li>
         <li>Linux</li>
-      </ul>
+      </ul> */}
       <p className="about_me_text">
         I am an aspiring front end web developer currently searching for a job.
         I graduated for the University of Washington in 2019 with a degree in
@@ -25,6 +26,49 @@ const AboutMe: React.FC = () => {
         Javascript and react. Currently I am living in Hawaii working on Tour
         boats as a deckhand and swim guide.
       </p>
+      <div id="experience_container">
+        <div className="experience_card">
+          <h2>Physics Tutor</h2>
+          <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Magni
+            voluptas ab placeat fugit, perspiciatis consectetur, id quia
+          </p>
+          <h3>Skills</h3>
+          <ul>
+            <li>Critical thinking</li>
+            <li>Problem solving</li>
+            <li></li>
+          </ul>
+        </div>
+        <div className="experience_card">
+          <h2>Safety diver</h2>
+          <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Magni
+            voluptas ab placeat fugit, perspiciatis consectetur, id quia
+          </p>
+          <h3>Skills</h3>
+
+          <ul>
+            <li>Working under pressure</li>
+            <li>Managing expectations</li>
+            <li>Managing multiple people</li>
+          </ul>
+        </div>
+        <div className="experience_card">
+          <h2>Plasma Physics Researcher</h2>
+          <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Magni
+            voluptas ab placeat fugit, perspiciatis consectetur, id quia
+          </p>
+          <h3>Skills</h3>
+
+          <ul>
+            <li>Working under deadlines</li>
+            <li>Tackling real life technical problems</li>
+            <li>Compiling results into a cohesive paper</li>
+          </ul>
+        </div>
+      </div>
     </section>
   );
 };
