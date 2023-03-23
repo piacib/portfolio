50c25506eea7043161f651b827785c304d664540
piacib <piacib>
2023-03-22 17:22:46 -0700

    moved svg to children prop

--- src/components/Bubble/Bubble.tsx
+++ src/components/Bubble/Bubble.tsx
@@ -5,26 +5,16 @@ import "./bubble-background.css";
 import "./bubble.mediaquery.css";
 
 interface BubbleProps {
-  imageSrc: string;
-  imageAlt: string;
-  text: string;
-  href: string;
-  firstBubble?: boolean;
+  href?: string;
+  children?: JSX.Element;
   onClick?: () => void;
 }
-const Bubble: React.FC<BubbleProps> = ({
-  imageSrc,
-  imageAlt,
-  text,
-  href,
-  firstBubble = false,
-  onClick,
-}) => {
+const Bubble: React.FC<BubbleProps> = ({ href, onClick, children }) => {
   return (
     <li className="bubble" onClick={onClick}>
       <a href={href}>
         <span className="dynamic-backgrnd"></span>
-        <img className="bubble-image" src={imageSrc} alt={imageAlt} />
+        {children}
       </a>
     </li>
   );
