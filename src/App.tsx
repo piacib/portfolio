b089c7c6fa232b6ab41db689dcc348b51626b9dc
piacib <piacib>
2023-04-05 13:47:32 -0700

    moved content to home folder

--- src/App.tsx
+++ src/App.tsx
@@ -2,30 +2,21 @@ import React from "react";
 import "./App.css";
 import "./normalize.css";
 import "./fonts.css";
-import "./home.css";
-import FerrisWheel from "./components/FerrisWheel/FerrisWheel";
-import Toggle from "./components/Toggle/Toggle";
 import AboutMe from "./components/AboutMe/AboutMe";
 import Projects from "./components/Projects/Projects";
-import { useMode } from "./utils/useMode";
-import Grass from "./components/Grass/Grass";
+import Home from "./components/Home/Home";
+import LoadScreen from "./components/PageLoadAnimation/LoadScreen";
+window.onbeforeunload = function () {
+  window.scrollTo(0, 0);
+};
 function App() {
-  const { togClass, handleChange } = useMode();
   return (
     <>
-      <section id="home">
-        <header>
-          <Toggle togClass={togClass} handleChange={handleChange} />
-        </header>
-        <div className="home_text_container">
-          <h2>Hi my name is,</h2>
-          <h1>Ben Piacitelli</h1>
-          <h2>I am a front end web developer</h2>
-        </div>
-        <FerrisWheel />
-      </section>
-      <AboutMe />
-      <Projects />
+      <LoadScreen />
+      <Home />
+      {/* <AboutMe /> */}
+      {/*
+      <Projects /> */}
     </>
   );
 }
