e171bd2ac0b344c9e357c72469de462d2d6ec6bf
piacib <piacib>
2023-04-13 09:57:03 -0700

    added threshold prop

--- src/utils/useIntersectionObserver.ts
+++ src/utils/useIntersectionObserver.ts
@@ -1,30 +1,35 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useEffect, useRef, useState } from "react";
 
-const useIntersectionObserver = (ref:React.RefObject<HTMLElement>) => {
-        const [isOnScreen, setIsOnScreen] = useState(false);
-        const observerRef = useRef<null | IntersectionObserver>(null);
-        useEffect(() => {
-          observerRef.current = new IntersectionObserver(([entry]) =>
-            setIsOnScreen(entry.isIntersecting)
-          );
-        }, []);
-      
-        useEffect(() => {
-            if (!observerRef.current) return
-            if (ref.current) {
+const useIntersectionObserver = (
+  ref: React.RefObject<HTMLElement>,
+  threshold = 0.75
+) => {
+  const [isOnScreen, setIsOnScreen] = useState(false);
+  const observerRef = useRef<null | IntersectionObserver>(null);
+  useEffect(() => {
+    console.log("adding IntersectionObserver");
 
-                observerRef.current.observe(ref.current);
-            } 
-      
-          return () => {
-            if (!observerRef.current) return
-            observerRef.current.disconnect();
-          };
-        }, [ref]);
-      
-        return isOnScreen;
-      }
-      
+    observerRef.current = new IntersectionObserver(
+      ([entry]) => {
+        setIsOnScreen(entry.isIntersecting);
+      },
+      { threshold: threshold }
+    );
+  }, [threshold]);
 
+  useEffect(() => {
+    if (!observerRef.current) return;
+    if (ref.current) {
+      observerRef.current.observe(ref.current);
+    }
+
+    return () => {
+      if (!observerRef.current) return;
+      observerRef.current.disconnect();
+    };
+  }, [ref]);
+
+  return isOnScreen;
+};
 
 export default useIntersectionObserver;
