853719bb05112b7fe7061c2fc468411cd6e07aa1
piacib <ben.piacitelli@gmail.com>
2023-05-16 12:21:06 -0700

    removed scroll lock

--- src/App.tsx
+++ src/App.tsx
@@ -7,14 +7,13 @@ import AboutMe from "./components/AboutMe/AboutMe";
 import Projects from "./components/Projects/Projects";
 import Home from "./components/Home/Home";
 import LoadScreen from "./components/PageLoadAnimation/LoadScreen";
-window.onload = function () {
-  window.scrollTo(0, 0);
-};
+
 function App() {
   return (
     <>
       <LoadScreen />
       <Home />
+
       <AboutMe />
       <Projects />
     </>
