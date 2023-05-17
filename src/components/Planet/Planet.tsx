76090e3beda6b7a9af5c0f7dc80aa216e79a9c85
piacib <ben.piacitelli@gmail.com>
2023-05-17 13:26:38 -0700

    added intersection observer

--- src/components/Planet/Planet.tsx
+++ src/components/Planet/Planet.tsx
@@ -2,9 +2,8 @@ import { useState } from "react";
 import "./planet.scss";
 import variables from "../Moon/style.module.scss";
 import Moon from "../Moon/Moon";
-interface PlanetProps {
-  children?: JSX.Element[];
-}
+import useDualFadeIn from "../../utils/useDualFadeIn";
+
 export const icons = [
   "devicon-javascript-plain",
   "devicon-html5-plain-wordmark",
@@ -16,14 +15,16 @@ export const icons = [
   "devicon-typescript-plain",
   "devicon-sass-original",
 ];
-const Planet = ({ children }: PlanetProps) => {
+const Planet = () => {
+  const { classes: techsClasses, containerRef: techsRef } = useDualFadeIn();
+
   if (icons.length !== Number(variables.moonCount)) {
     console.warn(
       "not all icons are displayed, increase $moonCount to update Moons."
     );
   }
   return (
-    <div className={`orbit `}>
+    <div ref={techsRef} className={`orbit ${techsClasses.left} `}>
       <div className={`planet_face flip-containe circle `}>
         <p className="techs_text">Technologies</p>
       </div>
